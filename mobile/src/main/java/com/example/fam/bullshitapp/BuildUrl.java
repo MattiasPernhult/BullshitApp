4b8f7acb7c448f799ff26aaf28b78fd8297404fa
Mattias Pernhult <pernhultmattias@gmail.com>
2015-10-13 09:21:28 +0200

    Added BuilderUrl and ApiKeys credentials

--- mobile/src/main/java/com/example/fam/bullshitapp/BuildUrl.java
+++ mobile/src/main/java/com/example/fam/bullshitapp/BuildUrl.java
@@ -3,10 +3,8 @@ package com.example.fam.bullshitapp;
 /**
  * Created by mattiaspernhult on 2015-10-13.
  */
-public class Controller {
+public class BuildUrl {
 
-    public Controller() {
 
-    }
 
 }
---
+++ mobile/src/main/java/com/example/fam/bullshitapp/credentials/ApiKeys.java
@@ -0,0 +1,10 @@
+package com.example.fam.bullshitapp.credentials;
+
+/**
+ * Created by mattiaspernhult on 2015-10-13.
+ */
+public class ApiKeys {
+
+
+
+}
